09d860d8c36a268ee053532ae6bef37825b17c91
alboan <andreiinculet@yahoo.com>
2018-05-20 20:09:27 +0300

    adaugat orasele

--- MyMaps-teza/main.cpp
+++ MyMaps-teza/main.cpp
@@ -2,9 +2,11 @@
 #include<iostream>
 #include<vector>
 #include<fstream>
+#include<math.h>
 using namespace sf;
 using namespace std;
 fstream fisier;
+fstream distante;
 Event event;
 Texture Harta;
 Sprite HartaFundal;
@@ -17,7 +19,21 @@ struct structuraOras
     int x,y,factor,dim_text;
     string nume;
 };
+struct Linie
+{
+    Vector2f pos1;
+    Vector2f pos2;
+};
+vector<Linie> Linii;
+struct Drum
+{
+    string oras1;
+    string oras2;
+    int distanta;
+};
+structuraOras oras;
 vector<structuraOras> OraseCitite;
+vector<Drum> OraseSiDistante;
 class Oras
 {
 private: //ce i privat nu poate fi accesat din afara clasei
@@ -25,8 +41,8 @@ private: //ce i privat nu poate fi accesat din afara clasei
     {
         MarcajOras.setPosition(pos_x,pos_y); // setam pozitia marcajului orasului,am setat pozitia cercului
         MarcajOras.setSize(Vector2f(25,25));//setam lungimea si latimea marcajului orasului
-        CentruMarcaj.x=pos_x+5;
-        CentruMarcaj.y=pos_y+5;
+        CentruMarcaj.x=pos_x+10;
+        CentruMarcaj.y=pos_y+10;
         TexturaMarcaj.loadFromFile(cale);
         MarcajOras.setTexture(&TexturaMarcaj);
         InDreptulMarcajului.x=CentruMarcaj.x+factor;
@@ -43,8 +59,8 @@ private: //ce i privat nu poate fi accesat din afara clasei
     RectangleShape MarcajOras;
     Texture TexturaMarcaj;
     Vector2i CentruMarcaj,InDreptulMarcajului;
-    Text NumeText;
 public:
+    Text NumeText;
     void SeteazaPozaMarcaj(string cale)
     {
         TexturaMarcaj.loadFromFile(cale);
@@ -53,7 +69,14 @@ public:
     void DeseneazaOras()
     {
         fereastra.draw(MarcajOras);
-        fereastra.draw(NumeText);
+    }
+    bool MousePeste()
+    {
+        if(MarcajOras.getGlobalBounds().contains(fereastra.mapPixelToCoords(Mouse::getPosition(fereastra)))) // daca mouse ul este in dreptul marcajului unu oras
+        {
+            return true;
+        }
+        return false;
     }
     string Nume;
     Oras(int poz_x,int poz_y,string nume,string poza,int ffactor,int dim_text)
@@ -75,14 +98,48 @@ Oras CreeazaOras(int poz_x,int poz_y,string nume,string poza,int ffactor,int dim
 }
 void DeseneazaOrase()
 {
-    for(int i=0;i<Orase.size();i++)
+    for(int i=0; i<Orase.size(); i++)
     {
         Orase[i].DeseneazaOras();
+        if(Orase[i].MousePeste())
+        {
+            fereastra.draw(Orase[i].NumeText);
+        }
+    }
+}
+void CreeazaLinie(Vector2f pos1, Vector2f pos2)
+{
+    Linie l;
+    l.pos1=pos1;
+    l.pos2=pos2;
+    Linii.push_back(l);
+}
+void DeseneazaLinii()
+{
+    for(int i =0; i<Linii.size(); i++)
+    {
+        Vertex line2[] =
+        {
+            Vertex(Linii[i].pos1, Color::Red),
+            Vertex(Linii[i].pos2, Color::Red)
+        };
+        Vertex line3[]=
+        {
+            Vertex(Vector2f(Linii[i].pos1.x+1,Linii[i].pos1.y),Color::Red),
+            Vertex(Vector2f(Linii[i].pos2.x+1,Linii[i].pos2.y),Color::Red)
+        };
+        Vertex line4[]=
+        {
+            Vertex(Vector2f(Linii[i].pos1.x+2,Linii[i].pos1.y),Color::Red),
+            Vertex(Vector2f(Linii[i].pos2.x+2,Linii[i].pos2.y),Color::Red)
+        };
+        fereastra.draw(line2, 2, Lines);
+        fereastra.draw(line3,2,Lines);
+        fereastra.draw(line4,2,Lines);
     }
 }
 void CitesteOraseleDinFisier()
 {
-    cout<<"da";
     int x,y,factor,dim_text;
     string nume;
     fisier.open("orase.in",ios::in);
@@ -100,10 +157,25 @@ void CitesteOraseleDinFisier()
     fisier.close();
     fisier.clear();
 }
+void CitesteDistantele()
+{
+    Drum d;
+    distante.open("distante.in",ios::in);
+    while(distante>>d.oras1>>d.oras2>>d.distanta)
+    {
+        OraseSiDistante.push_back(d);
+    }
+    distante.close();
+    distante.clear();
+    for(int i=0; i<OraseSiDistante.size(); i++)
+    {
+        cout<<d.oras1<<" "<<d.oras2<<" "<<d.distanta<<endl;
+    }
+}
 void AfiseazaOraseleInFisier()
 {
     fisier.open("orase.in",ios::out);
-    for(int i=0;i<OraseCitite.size();i++)
+    for(int i=0; i<OraseCitite.size(); i++)
     {
         fisier<<OraseCitite[i].x<<" "<<OraseCitite[i].y<<" "<<OraseCitite[i].nume<<" "<<OraseCitite[i].factor<<" "<<OraseCitite[i].dim_text<<'\n';
     }
@@ -117,13 +189,14 @@ int main()
     HartaFundal.setTexture(Harta);//setam textura sprite-ului
     fereastra.create(VideoMode(1000, 800), "MyMaps");
     CitesteOraseleDinFisier();
-    for(int i=0; i<Orase.size();i++)
+    CitesteDistantele();
+    for(int i=0; i<Orase.size(); i++)
     {
         Orase[i].SeteazaPozaMarcaj("poze/PunctOrasMarcat.png");
     }
     while (fereastra.isOpen())
     {
-        cout<<Mouse::getPosition(fereastra).x<<" "<<Mouse::getPosition(fereastra).y<<endl;
+        //cout<<Mouse::getPosition(fereastra).x<<" "<<Mouse::getPosition(fereastra).y<<endl;
         while (fereastra.pollEvent(event))
         {
             if (event.type == Event::Closed)//verific daca am apasat iconita de inchis fereastra
@@ -134,6 +207,7 @@ int main()
         fereastra.clear();
         fereastra.draw(HartaFundal);//desenam sprite-ul (ca sa il vedem)
         DeseneazaOrase();
+        DeseneazaLinii();
         fereastra.display();
     }
     AfiseazaOraseleInFisier();
